cf92adf1704ffff25625e2587d369249f6dc6c3c
gstark0 <gstark0@icloud.com>
2020-07-17 22:15:16 +0200

    Add a CollectionView

---
+++ MobileFork/Explore/ExploreViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ExploreViewController.swift
+//  LetsEat
+//
+//  Created by Greg Stark on 06/07/2020.
+//  Copyright © 2020 Gregory Stark. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath)
+    -> UICollectionReusableView {
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                 withReuseIdentifier: "header", for: indexPath)
+        return headerView
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for: indexPath)
+        return cell
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
