10827519e55fbfc4b11d31484ec2a36d0b5ecedf
gstark0 <gstark0@icloud.com>
2020-07-22 10:28:14 +0200

    Add restaurant details screen

---
+++ Restaurants/RestaurantListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RestaurantListViewController.swift
+//  LetsEat
+//
+//  Created by Greg Stark on 22/07/2020.
+//  Copyright © 2020 Gregory Stark. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantListViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "restaurantCell", for: indexPath)
+    }
+
+}
