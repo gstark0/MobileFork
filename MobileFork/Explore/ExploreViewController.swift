ab3d748c6a1869c08273dc9ae668f69d02cedab8
gstark0 <gstark0@icloud.com>
2020-07-27 16:13:58 +0200

    Render 'Explore' data from plist file

--- MobileFork/Explore/ExploreViewController.swift
+++ MobileFork/Explore/ExploreViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ExploreViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     @IBOutlet weak var collectionView: UICollectionView!
-    
+    let manager = ExploreDataManager()
     @IBAction func unwindLocationCancel(segue:UIStoryboardSegue){
         
     }
@@ -24,11 +24,14 @@ class ExploreViewController: UIViewController, UICollectionViewDataSource, UICol
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return manager.numberOfItems()
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for: indexPath) as! ExploreCell
+        let item = manager.explore(at: indexPath)
+        cell.lblName.text = item.name
+        cell.imgExplore.image = UIImage(named: item.image)
         return cell
     }
     
@@ -36,6 +39,6 @@ class ExploreViewController: UIViewController, UICollectionViewDataSource, UICol
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.manager.fetch()
     }
 }
---
+++ MobileFork/Explore/View/ExploreCell.swift
@@ -0,0 +1,15 @@
+//
+//  ExploreCell.swift
+//  LetsEat
+//
+//  Created by Greg Stark on 27/07/2020.
+//  Copyright © 2020 Gregory Stark. All rights reserved.
+//
+
+import UIKit
+
+class ExploreCell: UICollectionViewCell {
+    @IBOutlet weak var lblName: UILabel!
+    
+    @IBOutlet weak var imgExplore: UIImageView!
+}
